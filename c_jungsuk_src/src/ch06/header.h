a052d7377f26186367bbd2f0859deaae374bc635
castello <castello@naver.com>
2018-02-20 01:24:53 +0900

    1st upload
    delete redundant files

--- c_jungsuk_src/src/ch06/header.h
+++
@@ -1,5 +0,0 @@
-int  multiply(int x, int y);                 
-int  getUserInput(void);                     
-void printGugudan(int dan);                  
-void printGugudanAll(void);                  
-                                             
